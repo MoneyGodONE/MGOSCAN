f0b75a24623e8aae4dbbdf81cc58c29eba9142f8
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-10 03:04:37 +0700

    Refactor Footer component to remove duplication
    Removed duplicate Footer component definition and simplified Props type declaration.

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,21 +1,9 @@
 import React from "react";
 
-type Props = {
-  updated: string;
-};
+type Props = { updated: string };
 
 const Footer: React.FC<Props> = ({ updated }) => (
   <div>Last Updated: {updated || "N/A"}</div>
 );
 
 export default Footer;
-
-type FooterProps = { updated: string };
-
-const Footer: React.FC<FooterProps> = ({ updated }) => (
-  <div>Last Updated: {updated || "N/A"}</div>
-);
-
-export default Footer;
-
-
