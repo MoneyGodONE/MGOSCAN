fb3f8041bded85aa87718121a7b3efd91c0a879c
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-10 03:03:13 +0700

    Update TokenInfoCard.tsx

--- src/ui/components/TokenInfoCard.tsx
+++ src/ui/components/TokenInfoCard.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { TokenData } from "../App";
 
-type Props = {
-  data: TokenData;
-};
+type Props = { data: TokenData };
 
 const TokenInfoCard: React.FC<Props> = ({ data }) => (
   <div>
     <h2 className="text-xl font-bold">{data.name || "MGO"}</h2>
     <p>Symbol: {data.symbol}</p>
     <p>Total Supply: {data.totalSupply}</p>
-    <p>Price USD: {data.price_usd}</p>
+    <p>Price USD: {data.price_usd ?? "N/A"}</p>
+    <p>Market Cap: {data.market_cap_usd ?? "N/A"}</p>
   </div>
 );
 
