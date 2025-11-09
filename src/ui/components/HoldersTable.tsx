8ba724d8d41652774e52d515bb5ebcb9a49aee52
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-10 02:42:01 +0700

    Create HoldersTable.tsx

---
+++ src/ui/components/HoldersTable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type Holder = {
+  tokenAccount: string;
+  owner: string | null;
+  amount: string;
+};
+
+type Props = {
+  holders: Holder[];
+};
+
+const HoldersTable: React.FC<Props> = ({ holders }) => (
+  <table className="w-full text-left border-collapse">
+    <thead>
+      <tr>
+        <th>Account</th>
+        <th>Owner</th>
+        <th>Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      {holders.map((h, idx) => (
+        <tr key={idx}>
+          <td>{h.tokenAccount}</td>
+          <td>{h.owner || "N/A"}</td>
+          <td>{h.amount}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+export default HoldersTable;
