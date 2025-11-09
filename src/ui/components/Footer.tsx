ac71636862b8281608c3170028162197cabce2fe
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-10 02:43:56 +0700

    Add Footer component to display last updated date

---
+++ src/ui/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  updated: string;
+};
+
+const Footer: React.FC<Props> = ({ updated }) => (
+  <div>Last Updated: {updated || "N/A"}</div>
+);
+
+export default Footer;
