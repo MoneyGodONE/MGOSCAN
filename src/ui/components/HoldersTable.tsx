48cd26827aa65e2f1ceedaf52a51866050ce9601
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-14 00:49:47 +0700

    Update HoldersTable.tsx

--- src/ui/components/HoldersTable.tsx
+++ src/ui/components/HoldersTable.tsx
@@ -1,29 +1,40 @@
 import React from "react";
 import { Holder } from "../App";
 
-type Props = { holders: Holder[] };
+interface Props {
+  holders: Holder[];
+  decimals: number;
+  totalSupplyRaw: string;
+}
 
-const HoldersTable: React.FC<Props> = ({ holders }) => (
-  <table className="w-full text-left border-collapse">
-    <thead>
-      <tr>
-        <th>Account</th>
-        <th>Owner</th>
-        <th>Amount</th>
-        <th>Percent</th>
-      </tr>
-    </thead>
-    <tbody>
-      {holders.map((h, idx) => (
-        <tr key={idx}>
-          <td>{h.tokenAccount}</td>
-          <td>{h.owner}</td>
-          <td>{h.amount}</td>
-          <td>{h.percent}</td>
+const HoldersTable: React.FC<Props> = ({ holders, decimals, totalSupplyRaw }) => {
+  const formatAmount = (raw: string) => (Number(raw) / 10 ** decimals).toLocaleString();
+
+  const formatPercent = (raw: string) =>
+    totalSupplyRaw ? ((Number(raw) / Number(totalSupplyRaw)) * 100).toFixed(2) + "%" : "0%";
+
+  return (
+    <table className="w-full text-left border-collapse">
+      <thead>
+        <tr>
+          <th>Account</th>
+          <th>Owner</th>
+          <th>Amount</th>
+          <th>Percent</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {holders.map((h, idx) => (
+          <tr key={idx}>
+            <td>{h.tokenAccount}</td>
+            <td>{h.owner}</td>
+            <td>{formatAmount(h.rawAmount)}</td>
+            <td>{formatPercent(h.rawAmount)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default HoldersTable;
