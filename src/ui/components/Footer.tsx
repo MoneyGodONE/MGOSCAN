c0623d69397ea3a00910a70102b2ead934276b47
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-14 02:27:02 +0700

    Update Footer.tsx

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 type Props = { updated: string };
 
-const Footer: React.FC<Props> = ({ updated }) => <div>Last Updated: {updated}</div>;
+const Footer: React.FC<Props> = ({ updated }) => (
+  <div>Last Updated: {updated || "N/A"}</div>
+);
 
 export default Footer;
