98cc3cc7cd9f405d0d64a3ac614c46ec27bd4e26
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-14 01:22:13 +0700

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,20 @@
 export type Holder = {
-tokenAccount: string;
-owner: string | null;
-rawAmount: string;
-amount: string; // formatted with decimals
+  tokenAccount: string;
+  owner: string | null;
+  rawAmount: string;
+  amount: string; // formatted with decimals
 };
 
-
 export type TokenData = {
-mint: string;
-name?: string | null;
-symbol?: string | null;
-decimals?: number | null;
-totalSupplyRaw?: string | null;
-totalSupply?: string | null;
-holders: Holder[];
-price_usd?: number | null;
-market_cap_usd?: number | null;
-price_updated?: string | null;
-updated: string; // ISO
+  mint: string;
+  name?: string | null;
+  symbol?: string | null;
+  decimals?: number | null;
+  totalSupplyRaw?: string | null;
+  totalSupply?: string | null;
+  holders: Holder[];
+  price_usd?: number | null;
+  market_cap_usd?: number | null;
+  price_updated?: string | null;
+  updated: string; // ISO
 };
