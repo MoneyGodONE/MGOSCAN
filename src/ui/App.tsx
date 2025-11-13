d330ac8c81d0b2e8b19cc61cc66e238d1044f3ca
Money God ONE [MGO] <moneygodone@outlook.com>
2025-11-14 01:19:43 +0700

    Update App.tsx

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import TokenInfoCard from "./components/TokenInfoCard";
 import HoldersTable from "./components/HoldersTable";
 import Footer from "./components/Footer";
-import type { TokenData } from "../../types";
+import type { TokenData } from "../types"; // <- correct import
 
 const AppUI: React.FC = () => {
   const [token, setToken] = useState<TokenData | null>(null);
